2a13ddd3199d6ad11c04f4ce88a49cd895f8955c
Beniamin Muresan <muresan.beniamin@hotmail.com>
2023-04-19 17:31:11 +0300

    Add Rubocop

--- config/initializers/languages.rb
+++ config/initializers/languages.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 require 'csv'
 
 ACCEPTED_LANGUAGE_CODES = CSV.read('config/language-codes.csv').map do |lang_attrs|
   { code: lang_attrs.first, contry: lang_attrs.second }
 end
-
