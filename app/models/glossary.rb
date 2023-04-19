3d17acc6251f12fcb2d63cfe98be46da76f93586
Beniamin Muresan <muresan.beniamin@hotmail.com>
2023-04-19 23:17:23 +0300

    Create glossary migration & model

---
+++ app/models/glossary.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Glossary < ApplicationRecord
+  validates :source_language_code, uniqueness: { scope: :target_language_code,
+                                                 message: 'scope with target language code already taken' }
+  validates :source_language_code, inclusion: { in: ACCEPTED_LANGUAGE_CODES.pluck(:code),
+                                                message: 'is not a valid language code' }
+  validates :target_language_code, inclusion: { in: ACCEPTED_LANGUAGE_CODES.pluck(:code),
+                                                message: 'is not a valid language code' }
+end
